d6701c5068f21ddd07b2408e90800cc1b0544d9c
Idowu Akinde <8018104+idowuakinde@users.noreply.github.com>
2018-12-03 00:39:52 +0100

    First commit of the boilerplate project that comes out of the box with Asp.Net Core 2.1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hwapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Asp.Net Core 2 on Mac OSX does work!!");
+        }
+    }
+}
